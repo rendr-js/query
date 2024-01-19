a6b6896c4615705160b6d71715eeee49f589bfb5
= <harmtrav@gmail.com>
2024-01-19 16:56:48 -0600

    Remove unneeded named funcs

--- src/index.ts
+++ src/index.ts
@@ -69,33 +69,6 @@ export const api = (config: ApiConfig): Api => {
 
   const cacheAtom = createAtom<CachedResponses>({});
 
-  const executeFetch = <Resp>(input: Parameters<typeof fetch>[0], init: Parameters<typeof fetch>[1], setResponse: Dispatch<SetStateAction<QueryResponse<Resp>>>, setCache: Dispatch<SetStateAction<CachedResponses>>, invalidates: Set<string>) => {
-    fetchFn(input, init).then(r => r.json()).then((data: Resp) => {
-      setCache(cache => {
-        for (const key in cache) {
-          const tags = cache[key]!.tags;
-          for (const tag of invalidates) {
-            if (tags.has(tag)) {
-              delete cache[key]
-              return { ...cache };
-            }
-          }
-        }
-        return cache;
-      });
-      setResponse({ loading: false, data });
-    });
-  };
-
-  const executeCachedFetch = <Resp>(key: string, input: Parameters<typeof fetch>[0], init: Parameters<typeof fetch>[1], setCache: Dispatch<SetStateAction<CachedResponses>>, tags: Set<string>) => {
-    fetchFn(input, init).then(r => r.json()).then((data: Resp) => {
-      setCache(cache => ({
-        ...cache,
-        [key]: { resp: { data, loading: false }, tags },
-      }));
-    });
-  };
-
   const generate = (method: string): RequestHookGenerator => {
     return <Input, Resp>(cfg: RequestConfig<Input>) => {
       const invalidates = new Set(cfg.invalidates)
@@ -103,8 +76,21 @@ export const api = (config: ApiConfig): Api => {
         const setCache = useAtomSetter(cacheAtom);
         const [response, setResponse] = useState<QueryResponse<Resp>>({ loading: true, data: null });
         useEffect(() => {
-          const [inpt, int] = makeFetchArgs(cfg.request(input), method);
-          executeFetch(inpt, int, setResponse, setCache, invalidates);
+          fetchFn(...makeFetchArgs(cfg.request(input), method)).then(r => r.json()).then((data: Resp) => {
+            setCache(cache => {
+              for (const key in cache) {
+                const tags = cache[key]!.tags;
+                for (const tag of invalidates) {
+                  if (tags.has(tag)) {
+                    delete cache[key]
+                    return { ...cache };
+                  }
+                }
+              }
+              return cache;
+            });
+            setResponse({ loading: false, data });
+          })
         }, []);
         return response;
       };
@@ -119,8 +105,12 @@ export const api = (config: ApiConfig): Api => {
       useImmediateEffect(() => {
         if (!cache[key]) {
           cache[key] = { resp: { data: null, loading: true }, tags }
-          const [inpt, int] = makeFetchArgs(cfg.request(input), 'GET');
-          executeCachedFetch(key, inpt, int, setCache, tags);
+          fetchFn(...makeFetchArgs(cfg.request(input), 'GET')).then(r => r.json()).then((data: Resp) => {
+            setCache(cache => ({
+              ...cache,
+              [key]: { resp: { data, loading: false }, tags },
+            }));
+          });
         }
       }, [key]);
       return cache[key]!.resp;
